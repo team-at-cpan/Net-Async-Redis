8437993368e9898cd1e09081f0e5f8f55de9940e
Tom Molesworth <TEAM@cpan.org>
2017-12-26 17:58:52 +0800

    Include VERSION

--- lib/Net/Async/Redis/Server.pm
+++ lib/Net/Async/Redis/Server.pm
@@ -5,9 +5,15 @@ use warnings;
 
 use parent qw(IO::Async::Notifier);
 
+# VERSION
+
 =head1 NAME
 
 Net::Async::Redis::Server - basic server implementation
+
+=head1 DESCRIPTION
+
+Best to wait until the 2.000 release for this one.
 
 =cut
 
