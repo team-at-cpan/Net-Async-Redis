175ccaf362a29d06bb6bbe68ec524c13053432e5
Tom Molesworth <TEAM@cpan.org>
2021-02-13 19:43:03 +0800

    More clientside cache notifications - not just GET/SET

--- examples/cache-events.pl
+++ examples/cache-events.pl
@@ -10,7 +10,7 @@ use Syntax::Keyword::Try;
 use Future::Utils qw(fmap_void fmap_concat repeat);
 
 use Log::Any qw($log);
-use Log::Any::Adapter qw(Stdout), log_level => 'trace';
+use Log::Any::Adapter qw(Stdout), log_level => 'info';
 
 my $loop = IO::Async::Loop->new;
 
@@ -29,6 +29,14 @@ $log->infof('Set key');
 await $redis->set('clientside.cached' => 1);
 $log->infof('Get key');
 await $redis->get('clientside.cached');
+$log->infof('Apply more changes');
 await $redis->set('clientside.cached' => 2);
+await $redis->llen('clientside.cached.lpush');
+await $redis->lpush('clientside.cached.lpush' => 1,2,3);
+await $redis->hgetall('clientside.cached.hset');
+await $redis->hset('clientside.cached.hset' => abc => 123);
+await $redis->hset('clientside.cached.hset' => def => 123);
+await $redis->hset('clientside.cached.hset' => ghi => 123);
+$log->infof('Start loop');
 $loop->run;
 
